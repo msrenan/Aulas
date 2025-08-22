6c32f3f3ec96083a255f5dfd102326fbb811420c
Renan <machadosantosrenan@gmail.com>
2025-08-22 14:16:30 -0300

    First Commit

---
+++ AED_I/codigos/pile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define TAM 10
+
+class Pilha {
+    private:
+        int topo;
+        int data[TAM];
+    
+    public:
+
+        bool metodo() 
+}
